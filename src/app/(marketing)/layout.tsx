2a5f10bcd70a775e527255f351adcb7469a41b46
Didier Pereira <krrattoss5@gmail.com>
2026-06-04 20:37:19 -0500

    delete/businessPage:HideNavbar

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/src/components/Navbar"
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <main>{children}</main>
     </>
   )
